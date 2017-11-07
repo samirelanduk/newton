3d70d44653d0ac93162b7ae72a1258f1729d2154
Sam Ireland <sam.ireland.uk@gmail.com>
2017-11-07 11:58:43 +0000

    Start newton

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name="newton",
+      version="0.1.0",
+      description="Functions and calculus",
+      long_description="Tools for manipulating mathematical functions",
+      url="https://newton.samireland.com",
+      author="Sam Ireland",
+      author_email="mail@samireland.com",
+      license="MIT",
+      classifiers=["Development Status :: 4 - Beta",
+                   "Intended Audience :: Other Audience",
+                   "License :: OSI Approved :: MIT License",
+                   "Topic :: Communications",
+                   "Programming Language :: Python :: 3",
+                   "Programming Language :: Python :: 3.5",
+                   "Programming Language :: Python :: 3.6"],
+      packages=["newton"],
+      install_requires=[])
